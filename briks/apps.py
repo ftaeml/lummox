d9f635f9eb44fd45ca8eb12758486cc4dc5a0231
ftaeml <ftaeml@gmail.com>
2016-12-30 19:10:56 +0200

    ver.0.8 Видалено налаштувальний додатокю

--- briks/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BriksConfig(AppConfig):
-    name = 'briks'
--- lummox/urls.py
+++ lummox/urls.py
@@ -17,12 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from gallery import views
 
 urlpatterns = [
                   url(r'^admin/', admin.site.urls),
-                 # url(r'$', views.gallery, name="home"),
-                  #    url(r'^briks/', include('briks.urls')),
                   url(r'^blog/', include('blog.urls')),
                   url(r'^', include('gallery.urls')),
                   url(r'^info/', include('info.urls')),
